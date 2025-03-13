7c297619762b09979cecbf93a9f00bb9b35ad9eb
Adesokan segun <sheezycouture@gmail.com>
2025-03-13 15:16:00 +0100

    new repo

---
+++ python practice/function.py
@@ -0,0 +1,12 @@
+def addition(a, b):
+  return a + b
+
+def subtraction(a, b):
+  return a - b
+
+def multiplication(a, b):
+  return a * b
+
+print(addition(4, 5))
+print(subtraction(6, 9))
+print(multiplication(3, 7))
